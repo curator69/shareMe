04220b8cdb6d888d909ada8d3c410f94182426f2
curator69 <75662004+curator69@users.noreply.github.com>
2022-11-06 21:48:20 +0530

    added the loader spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Loader from "react-loader-spinner";
 
-const Spinner = () => {
-  return <div>Spinner</div>;
+const Spinner = ({ message }) => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Loader
+        type="Circles"
+        color="#00BFFF"
+        height={50}
+        width200
+        className="m-5"
+      />
+
+      <p className="text-lg textcenter px-2">{message}</p>
+    </div>
+  );
 };
 
 export default Spinner;
