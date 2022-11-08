d8d635c16528d159d73476a9e24ac0b4386bc278
curator69 <75662004+curator69@users.noreply.github.com>
2022-11-08 23:38:04 +0530

    completed userprofile part

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -31,6 +31,7 @@ const Feed = () => {
   if (loading)
     return <Spinner message="We are adding new ideas to your feed!" />;
 
+  if (!pins?.loading) return <h2>No pins available</h2>;
   return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 
 import logo from "../assets/logo.png";
+import { categories } from "../utils/data";
 
 const isNotActiveStyle =
   "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
@@ -11,13 +12,13 @@ const isNotActiveStyle =
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
-const Categories = [
-  { name: "Animals" },
-  { name: "Wallpapers" },
-  { name: "Photography" },
-  { name: "Gaming" },
-  { name: "Coding" },
-];
+// const Categories = [
+//   { name: "Animals" },
+//   { name: "Wallpapers" },
+//   { name: "Photography" },
+//   { name: "Gaming" },
+//   { name: "Coding" },
+// ];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -47,7 +48,7 @@ const Sidebar = ({ user, closeToggle }) => {
           <h3 className="mt-2 px-5 text-base 2xl:text-xl">
             Discover Categories
           </h3>
-          {Categories.slice(0, Categories.length - 1).map((category) => (
+          {categories.slice(0, categories.length - 1).map((category) => (
             <NavLink
               to={`/category/${category.name}`}
               className={({ isActive }) =>
@@ -56,6 +57,11 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+                alt="category"
+              />
               {category.name}
             </NavLink>
           ))}
